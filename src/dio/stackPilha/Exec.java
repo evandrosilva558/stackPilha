0cbcba8fd8a6c56b09235f7cbfff8a866a6c0c5a
Evandro <evandrosilva558@hotmail.com>
2022-01-05 18:20:04 -0300

    utilizado os metodos da pilha e verificado seus funcionamentos

--- src/dio/stackPilha/Exec.java
+++ src/dio/stackPilha/Exec.java
@@ -6,13 +6,27 @@ public class Exec {
 
 	public static void main(String[] args) {
 		
-		
+		// Stack pilha onde o ultimo adicionado e o primeiro a sair.
 		Stack<Carro> stackCarro = new Stack<>();
 		
-		stackCarro.add(new Carro("Ford"));
-		stackCarro.add(new Carro("Fiat"));
-		stackCarro.add(new Carro("Volkswagen"));
-
+		// objetos adicionados a pilha com metodo push.
+		stackCarro.push(new Carro("Ford"));
+		stackCarro.push(new Carro("Fiat"));
+		stackCarro.push(new Carro("Volkswagen"));
+		
+		// imprimindo a pilha de Carros.
+		System.out.println(stackCarro);
+		
+		// tirado um objeto do topo da pilha com metodo pop.
+		System.out.println(stackCarro.pop());
+		
+		// pega o ultimo objeto da pilha com metodo peek mais nao retira = Carro [marca=Fiat].
+		System.out.println(stackCarro.peek());
+		
+		System.out.println(stackCarro);
+		
+		// retorna se a pilha esta vazia com metodo empt = false.
+		System.out.println(stackCarro.empty());
 	}
 
 }
